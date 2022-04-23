8f98ad0b6e5050235650ffd09b1d535cd3bb540d
Martin Donovan <martindonovan3@gmail.com>
2022-04-22 21:24:11 -0400

    c

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ProductForm from './components/productForm';
-import FinalForm from './components/finalForm';
+import ProductForm from './components/product/productForm';
+import FinalForm from './components/product/finalForm';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { theme } from './theme';
 
@@ -10,9 +10,9 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        {/* <ProductForm /> */}
+        <ProductForm />
 
-        <FinalForm />
+        {/* <FinalForm /> */}
       </div>
     </ThemeProvider>
   );
--- src/components/product/productForm.js
+++ src/components/product/productForm.js
@@ -1,28 +1,29 @@
-import React, { useState } from 'react';
-import Autocomplete from '@mui/material/Autocomplete';
-import TextField from '@mui/material/TextField';
-import Input from '@mui/material/Input';
-import Checkbox from '@mui/material/Checkbox';
-import Slider from '@mui/material/Slider';
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
-import Container from '@mui/material/Container';
+import React, { useState } from "react";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
+import Input from "@mui/material/Input";
+import Checkbox from "@mui/material/Checkbox";
+import Slider from "@mui/material/Slider";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+import Container from "@mui/material/Container";
 
-import Paper from '@mui/material/Paper';
+import Paper from "@mui/material/Paper";
 //import './productForm.css';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-import { AppBar } from '@mui/material';
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { AppBar } from "@mui/material";
 
 const options = [
-  { label: 'Beaulieu Vineyards GDL Cabernet', id: 1 },
+  { label: "Beaulieu Vineyards GDL Cabernet", id: 1 },
   { label: "Cambria Julia's Vineyard Pinot Noir", id: 2 },
   { label: '"Desiderio" Avignonesi', id: 3 },
-  { label: 'Erath Pinot Gris', id: 4 },
-  { label: 'Foris Fly Over Red', id: 5 }
+  { label: "Erath Pinot Gris", id: 4 },
+  { label: "Foris Fly Over Red", id: 5 },
 ];
 
 export default function ProductForm() {
@@ -38,28 +39,28 @@ export default function ProductForm() {
           <Grid container spacing={10} alignItems="stretch">
             <Grid item xs={6}>
               <Autocomplete
-                style={{ marginTop: '10px', width: '100%' }}
+                style={{ marginTop: "10px", width: "100%" }}
                 disablePortal
                 id="combo-box-demo"
                 options={options}
                 sx={{ width: 300 }}
-                renderInput={params => (
+                renderInput={(params) => (
                   <TextField {...params} label="Product" />
                 )}
-                defaultValue={{ label: 'Select Product', value: 0 }}
+                defaultValue={{ label: "Select Product", value: 0 }}
               />
             </Grid>
             <Grid item xs={6}>
               <Autocomplete
-                style={{ marginTop: '10px', width: '100%' }}
+                style={{ marginTop: "10px", width: "100%" }}
                 disablePortal
                 id="combo-box-demo"
                 options={options}
                 sx={{ width: 300 }}
-                renderInput={params => (
+                renderInput={(params) => (
                   <TextField {...params} label="Product Instance" />
                 )}
-                defaultValue={{ label: 'Select Product Instance', value: 0 }}
+                defaultValue={{ label: "Select Product Instance", value: 0 }}
               />
             </Grid>
 
@@ -78,7 +79,7 @@ export default function ProductForm() {
                 defaultValue={70}
                 aria-label="Small"
                 valueLabelDisplay="auto"
-                style={{ width: '100px' }}
+                style={{ width: "100px" }}
               />
             </Grid>
             <Grid item xs={6}>
@@ -89,6 +90,17 @@ export default function ProductForm() {
                 placeholder="Placeholder"
                 multiline
                 variant="standard"
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextareaAutosize
+                maxRows={20}
+                minRows={20}
+                aria-label="maximum height"
+                placeholder="Maximum 4 rows"
+                defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua."
+                style={{ width: "100%" }}
               />
             </Grid>
           </Grid>
